51083733df954b7d30ccf1a2b19abea14c564314
Malikovich101 <73461252+Malikovich101@users.noreply.github.com>
2026-08-14 22:36:20 +0500

    Use explicit DigestPipeline in production

--- run_digest.py
+++ run_digest.py
@@ -1,11 +1,5 @@
-import digest
-import digest_policy
-import recent_memory_runtime
-
-
-recent_memory_runtime.install(digest)
-digest_policy.install(digest)
+from digest_pipeline import DigestPipeline
 
 
 if __name__ == "__main__":
-    digest.main()
+    DigestPipeline().run()
